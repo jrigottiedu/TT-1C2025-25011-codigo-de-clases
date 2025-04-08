ff78e8fd0e3bf6df29d939c17cba4497f1404a86
jrigottiedu <juan.rigotti@bue.edu.ar>
2025-04-07 23:19:30 -0300

    update clase 2

--- semana_2/clase_1/holaMundo.py
+++ semana_2/clase_1/holaMundo.py
@@ -1,1 +1,4 @@
-print("Hola Mundo")
+# la funcion print recibe algo
+# ese algo puede ser un valor "Hola mundo" es un texto
+# ese algo puede ser una variable
+print('Hola mundo')
--- semana_3/clase_2/hola_mundo.py
+++
@@ -1,4 +0,0 @@
-# la funcion print recibe algo
-# ese algo puede ser un valor "Hola mundo" es un texto
-# ese algo puede ser una variable
-print('Hola mundo')
