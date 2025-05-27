9dc878d3d3b1af02c13da937b1c94eb2432c8e35
jrigottiedu <juan.rigotti@bue.edu.ar>
2025-05-27 00:35:19 -0300

    update

---
+++ semana_10/clase_9/7_ejercicios.py
@@ -0,0 +1,31 @@
+# ------------------------------------------------
+# Ejercicio 1:
+
+"""
+Dada una lista sencilla de un producto
+
+producto = ["manzana", "fruta", 1200]
+
+Realizar una función que muestre en pantalla los 3 datos de nuestro producto
+
+"""
+
+# Resolución
+"""
+???
+"""
+
+# ------------------------------------------------
+# Ejercicio 2:
+
+"""
+Intentar convertir la opción 2 del menú en una función que muestre los valores de lista_producto
+
+La función puede o no recibir la lista_producto
+
+La función debe iterar la lista_producto y formatear la salida
+
+Apliquen colores de lo visto en el After Class de la semana 9
+https://github.com/jrigottiedu/TT-1C2025-25011-codigo-de-clases/blob/master/semana_9/after_class/aplicando_colores_consola.py
+
+"""
